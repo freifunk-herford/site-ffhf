99949fbfefe059d228b5f3d2237c9e67b5592094
iwok <snooper1337@gmail.com>
2015-10-06 23:34:05 +0200

    initial commit

---
+++ site.mk
@@ -0,0 +1,66 @@
+##	gluon site.mk makefile example
+
+##	GLUON_SITE_PACKAGES
+#		specify gluon/openwrt packages to include here
+#		The gluon-mesh-batman-adv-* package must come first because of the dependency resolution
+
+GLUON_SITE_PACKAGES := \
+	gluon-mesh-batman-adv-15 \
+	gluon-alfred \
+	gluon-announced \
+	gluon-autoupdater \
+	gluon-config-mode-autoupdater \
+	gluon-config-mode-contact-info \
+	gluon-config-mode-core \
+	gluon-config-mode-geo-location \
+	gluon-config-mode-hostname \
+	gluon-config-mode-mesh-vpn \
+	gluon-ebtables-filter-multicast \
+	gluon-ebtables-filter-ra-dhcp \
+	gluon-luci-admin \
+	gluon-luci-autoupdater \
+	gluon-luci-portconfig \
+	gluon-luci-wifi-config \
+	gluon-next-node \
+	gluon-mesh-vpn-fastd \
+	gluon-radvd \
+	gluon-setup-mode \
+	gluon-status-page \
+	haveged \
+	iptables \
+	iwinfo
+
+##	DEFAULT_GLUON_RELEASE
+#		version string to use for images
+#		gluon relies on
+#			opkg compare-versions "$1" '>>' "$2"
+#		to decide if a version is newer or not.
+
+DEFAULT_GLUON_RELEASE := 0.6+exp$(shell date '+%Y%m%d')
+
+
+##	GLUON_RELEASE
+#		call make with custom GLUON_RELEASE flag, to use your own release version scheme.
+#		e.g.:
+#			$ make images GLUON_RELEASE=23.42+5
+#		would generate images named like this:
+#			gluon-ff%site_code%-23.42+5-%router_model%.bin
+
+# Allow overriding the release number from the command line
+GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
+
+# Default priority for updates.
+# Specifies the maximum number of days until the update should be attempted
+# (thus lower numbers mean the priority is higher). It must be set either in 
+# site.mk or on the make manifest command line.
+# Updates will be attempted at night, between 04:00 and 5:00, with a specific
+# probability. When less than PRIORITY days have passed (calculated using 
+# DATE and the current time), the probability will proportional to the time 
+# passed. I.e. the update probability will start at 0 and slowly increase 
+# to 1 until PRIORITY days have passed. From then, the probability will be 
+# fixed at 1.
+
+GLUON_PRIORITY ?= 0.08
+
+# Languages to include
+GLUON_LANGS ?= de
