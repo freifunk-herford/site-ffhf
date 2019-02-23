7d5404ca5e403b694ae53cdb8c34e64641c80012
iwok <iwo@posteo.de>
2019-02-23 18:00:05 +0100

    Update site.mk

--- site.mk
+++ site.mk
@@ -18,6 +18,7 @@ GLUON_FEATURES := \
 GLUON_SITE_PACKAGES := \
     gluon-alfred \
     gluon-respondd \
+    gluon-autoupdater \
     gluon-config-mode-hostname \
     gluon-config-mode-autoupdater \
     gluon-config-mode-mesh-vpn \
